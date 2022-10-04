bf0b20f0167204401baa1a9748705d6f225bd9c0
Reymark <reymark.torres08.dev@gmail.com>
2022-10-04 21:13:16 +0800

    Add mysql imports

--- todolist.go
+++ todolist.go
@@ -3,9 +3,16 @@ package main
 import (
 	"io"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	log "github.com/sirupsen/logrus"
 )
+
+var db, _ = gorm.Open("mysql", "root:root@/todolist?charset=utf8&parseTime=True&loc=Local")
 
 func Healthz(w http.ResponseWriter, r *http.Request) {
 	log.Info("API Health is OK")
@@ -19,6 +26,8 @@ func init() {
 }
 
 func main() {
+	defer db.Close()
+
 	log.Info("Starting Todolist API server")
 	router := mux.NewRouter()
 	router.HandleFunc("/healthz", Healthz).Methods("GET")
