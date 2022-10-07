cb5125be9be7c587c37b430f73f1d989c43e00cd
Reymark <reymark.torres08.dev@gmail.com>
2022-10-07 22:05:05 +0800

    GetItemByID & UpdateItem functions

--- todolist.go
+++ todolist.go
@@ -3,8 +3,11 @@ package main
 import (
 	"io"
 	"net/http"
+	"strconv"
 
 	"encoding/json"
+
+	_ "strconv"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
@@ -35,6 +38,39 @@ func CreateItem(w http.ResponseWriter, r *http.Request) {
 	result := db.Last(&todo)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(result.Value)
+}
+
+func UpdateItem(w http.ResponseWriter, r *http.Request) {
+	// Get URK parameter from mux
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+
+	// Test if the TodoItem exist in DB
+	err := GetItemByID(id)
+
+	if err == false {
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{"updated": false, "error": "Record Not Found"}`)
+	} else {
+		completed, _ := strconv.ParseBool(r.FormValue("completed"))
+		log.WithFields(log.Fields{"Id": id, "Completed": completed}).Info("Updating TodoItem")
+		todo := &TodoItemModel{}
+		db.First(&todo, id)
+		todo.Completed = completed
+		db.Save(&todo)
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{"updated": true}`)
+	}
+}
+
+func GetItemByID(Id int) bool {
+	todo := &TodoItemModel{}
+	result := db.First(&todo, Id)
+	if result.Error != nil {
+		log.Warn("TodoItem not found in database")
+		return false
+	}
+	return true
 }
 
 func init() {
